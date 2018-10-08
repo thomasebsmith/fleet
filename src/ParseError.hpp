4166a37cbb3ad448273a7acd7d1f8de744266fb0
Thomas Smith <thsm@umich.edu>
2018-10-07 23:20:39 -0400

    Add header guard to ParseError.hpp

--- src/ParseError.hpp
+++ src/ParseError.hpp
@@ -1,5 +1,10 @@
+#ifndef PARSEERROR_HPP
+#define PARSEERROR_HPP
+
 class ParseError: public std::runtime_error {
 public:
   ParseError(std::string message);
   virtual const char *what() const throw();
 };
+
+#endif
