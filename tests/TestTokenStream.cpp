c4d422ba07a4d817ee38ec8e1e4bfcaded377b48
Thomas Smith <thsm@umich.edu>
2018-10-10 22:01:15 -0400

    Change tests to work with assertion macro

--- tests/TestTokenStream.cpp
+++ tests/TestTokenStream.cpp
@@ -38,52 +38,52 @@ void basicEndpointTokens() {
   Tester::confirm(basic.peek() == toPeek);
   Tester::confirm(basic.next() == toPeek);
   
-  Tester::confirm(basic.next() == Token { "_ba_r", Token::Type::Identifier });
-  Tester::confirm(basic.next() == Token { "1.3", Token::Type::Number });
-  Tester::confirm(basic.next() == Token {
+  Tester::confirm(basic.next() == (Token { "_ba_r", Token::Type::Identifier }));
+  Tester::confirm(basic.next() == (Token { "1.3", Token::Type::Number }));
+  Tester::confirm(basic.next() == (Token {
     "\"this \\\"is a test\"", Token::Type::String
-  });
+  }));
   Tester::confirm(!basic.hasNext());
 }
 
 void grouperTokens() {
   TokenStream groupers {"(( [foo bar]\t{}{  )"};
-  Tester::confirm(groupers.next() == Token { "(", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { "(", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { "[", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { "foo", Token::Type::Identifier });
-  Tester::confirm(groupers.next() == Token { "bar", Token::Type::Identifier });
-  Tester::confirm(groupers.next() == Token { "]", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { "{", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { "}", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { "{", Token::Type::Grouper });
-  Tester::confirm(groupers.next() == Token { ")", Token::Type::Grouper });
+  Tester::confirm(groupers.next() == (Token { "(", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { "(", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { "[", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { "foo", Token::Type::Identifier }));
+  Tester::confirm(groupers.next() == (Token { "bar", Token::Type::Identifier }));
+  Tester::confirm(groupers.next() == (Token { "]", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { "{", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { "}", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { "{", Token::Type::Grouper }));
+  Tester::confirm(groupers.next() == (Token { ")", Token::Type::Grouper }));
   Tester::confirm(!groupers.hasNext());
 }
 
 void allTokens() {
   TokenStream all
     {"F x=5. 3\n\n4 +-**a!\n 7(){1} bar_0 'foobar \\'is'#foo#a\n"};
-  Tester::confirm(all.next() == Token { "F", Token::Type::Identifier });
-  Tester::confirm(all.next() == Token { "x", Token::Type::Identifier });
-  Tester::confirm(all.next() == Token { "=", Token::Type::Operator });
-  Tester::confirm(all.next() == Token { "5.", Token::Type::Number });
-  Tester::confirm(all.next() == Token { "3", Token::Type::Number });
-  Tester::confirm(all.next() == Token { "\n", Token::Type::LineBreak });
-  Tester::confirm(all.next() == Token { "\n", Token::Type::LineBreak });
-  Tester::confirm(all.next() == Token { "4", Token::Type::Number });
-  Tester::confirm(all.next() == Token { "+-**", Token::Type::Operator });
-  Tester::confirm(all.next() == Token { "a", Token::Type::Identifier });
-  Tester::confirm(all.next() == Token { "!", Token::Type::Operator });
-  Tester::confirm(all.next() == Token { "7", Token::Type::Number });
-  Tester::confirm(all.next() == Token { "(", Token::Type::Grouper });
-  Tester::confirm(all.next() == Token { ")", Token::Type::Grouper });
-  Tester::confirm(all.next() == Token { "{", Token::Type::Grouper });
-  Tester::confirm(all.next() == Token { "1", Token::Type::Number });
-  Tester::confirm(all.next() == Token { "}", Token::Type::Grouper });
-  Tester::confirm(all.next() == Token { "bar_0", Token::Type::Identifier });
-  Tester::confirm(all.next() == Token { "'foobar \\'is'", Token::Type::String });
-  Tester::confirm(all.next() == Token { "#foo#a", Token::Type::Comment });
-  Tester::confirm(all.next() == Token { "\n", Token::Type::LineBreak });
+  Tester::confirm(all.next() == (Token { "F", Token::Type::Identifier }));
+  Tester::confirm(all.next() == (Token { "x", Token::Type::Identifier }));
+  Tester::confirm(all.next() == (Token { "=", Token::Type::Operator }));
+  Tester::confirm(all.next() == (Token { "5.", Token::Type::Number }));
+  Tester::confirm(all.next() == (Token { "3", Token::Type::Number }));
+  Tester::confirm(all.next() == (Token { "\n", Token::Type::LineBreak }));
+  Tester::confirm(all.next() == (Token { "\n", Token::Type::LineBreak }));
+  Tester::confirm(all.next() == (Token { "4", Token::Type::Number }));
+  Tester::confirm(all.next() == (Token { "+-**", Token::Type::Operator }));
+  Tester::confirm(all.next() == (Token { "a", Token::Type::Identifier }));
+  Tester::confirm(all.next() == (Token { "!", Token::Type::Operator }));
+  Tester::confirm(all.next() == (Token { "7", Token::Type::Number }));
+  Tester::confirm(all.next() == (Token { "(", Token::Type::Grouper }));
+  Tester::confirm(all.next() == (Token { ")", Token::Type::Grouper }));
+  Tester::confirm(all.next() == (Token { "{", Token::Type::Grouper }));
+  Tester::confirm(all.next() == (Token { "1", Token::Type::Number }));
+  Tester::confirm(all.next() == (Token { "}", Token::Type::Grouper }));
+  Tester::confirm(all.next() == (Token { "bar_0", Token::Type::Identifier }));
+  Tester::confirm(all.next() == (Token { "'foobar \\'is'", Token::Type::String }));
+  Tester::confirm(all.next() == (Token { "#foo#a", Token::Type::Comment }));
+  Tester::confirm(all.next() == (Token { "\n", Token::Type::LineBreak }));
   Tester::confirm(!all.hasNext());
 }
