14b22c2876e764334679e66b662959527eec28b0
Thomas Smith <thsm@umich.edu>
2018-11-11 19:59:49 -0500

    Add inline documentation for Evaluators

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -1,3 +1,8 @@
+// File: src/Evaluator.cpp
+// Purpose: Source file for Evaluators, which are tied to a Context and obtain
+// the result of evaluating some Fleet in that Context. See src/Evaluator.hpp
+// for more documentation.
+
 #include <stdexcept>
 #include <variant>
 #include <vector>
@@ -10,13 +15,24 @@
 #include "TokenTree.hpp"
 #include "Value.hpp"
 
+// This constructor creates an Evaluator with the given Context.
 Evaluator::Evaluator(const Context::Pointer &context):
   evaluationContext { context } {}
 
+// This method returns the result of evaluating the given TokenTree in the
+// evaluator's Context. It uses the fact that each Evaluator is a
+// TokenTreeVisitor (i.e. see the Evaluator::visit methods for further
+// implementation).
 Value::OrError Evaluator::evaluate(const TokenTree &ast) {
   bool wasRemoveContextLayer = removeContextLayer;
   removeContextLayer = false;
+
+  // Evaluate the tree using this class as a TokenTreeVisitor.
   const Value::OrError &result = ast.accept(*this);
+
+  // Remove the current Context and go to the parent Context if necessary.
+  // This should only be necessary if a variable was temporarily defined
+  // using the tempDefine method.
   if (wasRemoveContextLayer) {
     evaluationContext =
       Context::Pointer { evaluationContext->getParentContext() };
@@ -24,8 +40,13 @@ Value::OrError Evaluator::evaluate(const TokenTree &ast) {
   return result;
 }
 
+// This method defines a variable as a value for the next evaluation (i.e.
+// the next call to the evaluate method).
 void Evaluator::tempDefine(const std::string &name, Value::Pointer value) {
+  // Create a new Context with the current Context as the parent Context.
   Context::Pointer newContext { new Context { evaluationContext } };
+
+  // If there is an error, this function throws an error.
   if (newContext->define(name, value)) {
     throw ParseError { "Internal error: Could not define in function call" };
   }
@@ -33,6 +54,9 @@ void Evaluator::tempDefine(const std::string &name, Value::Pointer value) {
   removeContextLayer = true;
 }
 
+// This method accepts a Token and converts it into a Value Pointer or an error
+// if it cannot be converted. Note that strings conversions are not yet
+// implemented.
 Value::OrError Evaluator::visit(const Token &token) const {
   switch (token.getType()) {
     case Token::Type::Identifier:
@@ -48,13 +72,20 @@ Value::OrError Evaluator::visit(const Token &token) const {
   }
 }
 
+// This method accepts two TokenTrees (for a function and an argument) and
+// returns the result of calling the function with the argument if possible.
 Value::OrError Evaluator::visit(const TokenTree &f, const TokenTree &x) const {
+  // Evaluate the function TokenTree.
   const Value::OrError &fValueOrErr = f.accept(*this);
   if (std::holds_alternative<std::runtime_error>(fValueOrErr)) {
     return fValueOrErr;
   }
   const auto &fValue = *std::get_if<Value::Pointer>(&fValueOrErr);
 
+  // If the argument is implied (i.e. there is no first argument), return the
+  // reverse of the function. An implied function tree results from syntax
+  // like (+ 3), which returns the reverse of the (+) function with 3 as its
+  // first argument.
   if (x.isImplied()) {
     const auto &maybeReversible = std::dynamic_pointer_cast<
       ReversibleCallValue
@@ -72,15 +103,21 @@ Value::OrError Evaluator::visit(const TokenTree &f, const TokenTree &x) const {
     } };
   }
 
+  // If the argument is not implied, evaluate the argument TokenTree.
   const Value::OrError &xValueOrErr = x.accept(*this);
   if (std::holds_alternative<std::runtime_error>(xValueOrErr)) {
     return xValueOrErr;
   }
   const auto &xValue = *std::get_if<Value::Pointer>(&xValueOrErr);
 
+  // Return the result of calling the function with the argument.
   return fValue->call(xValue);
 }
 
+// This method converts a vector of TokenTrees into a Value Pointer. It
+// evaluates each line and returns the result of the last line if there is
+// no error. If there is an error, it returns the first error without
+// evaluating further lines.
 Value::OrError Evaluator::visit(const std::vector<TokenTree> &lines) const {
   // TODO: New Context?
   Value::OrError lastValue { ParseError { "Invalid empty code block " } };
@@ -93,6 +130,9 @@ Value::OrError Evaluator::visit(const std::vector<TokenTree> &lines) const {
   return lastValue;
 }
 
+// This method is used for visiting implied arguments. This should not occur,
+// since implied arguments should be detected when they are function
+// arguments (see visit(f, x)).
 Value::OrError Evaluator::visit() const {
   return { ParseError { "Internal error: Invalid implied argument in tree" } };
 }
--- src/Evaluator.hpp
+++ src/Evaluator.hpp
@@ -1,3 +1,8 @@
+// File: src/Evaluator.cpp
+// Purpose: Header file for Evaluators, which are tied to a Context and obtain
+// the result of evaluating some Fleet in that Context. See src/Evaluator.cpp
+// for implementations.
+
 #ifndef EVALUATOR_HPP
 #define EVALUATOR_HPP
 
@@ -9,17 +14,44 @@
 #include "TokenTreeVisitor.hpp"
 #include "Value.hpp"
 
+// The Evaluator class is a TokenTreeVisitor returning a type of Value::OrError.
+//  This means that it can visit a TokenTree (using tree.visit(*this)) and
+//  will always return a Value::OrError no matter the TokenTree. The accepting/
+//  visting paradigm should be used only internally. Use the evaluate(ast)
+//  method for code evaluation.
 class Evaluator: public TokenTreeVisitor<Value::OrError> {
 private:
   Context::Pointer evaluationContext;
   bool removeContextLayer = false;
 public:
+  // Constructor(context) - Creates an Evaluator with the given Context as its
+  //  evaluation Context (i.e. the context in which it will parse code).
   Evaluator(const Context::Pointer &context);
+
+  // evaluate(ast) - Evaluates the given TokenTree and returns either a Value
+  //  Pointer or an error depending on the result of the code.
   Value::OrError evaluate(const TokenTree &ast);
+
+  // tempDefine(name, value) - Temporarily (for only the next call of
+  //  evaluate(ast)) defines a variable with the given name and value. Note
+  //  that this creates a temporary child context rather than adding it to
+  //  the evaluation context directly.
   void tempDefine(const std::string &name, Value::Pointer value);
+
+  // visit(token) - Returns the result of converting the given Token to a Value.
   Value::OrError visit(const Token &token) const;
+
+  // visit(f, x) - Returns the result of evaluating the given function call.
   Value::OrError visit(const TokenTree &f, const TokenTree &x) const;
+
+  // visit(lines) - Returns the result of evaluating the given set of lines of
+  //  code. Note that the value of the last line is returned if there are no
+  //  errors, or the first error is returned if there are errors.
   Value::OrError visit(const std::vector<TokenTree> &lines) const;
+
+  // visit() - Returns an error, since this method is used for visiting implied
+  //  arguments, which should only exist within function calls and should be
+  //  parsed within function calls. This method should never be called.
   Value::OrError visit() const;
 };
 
