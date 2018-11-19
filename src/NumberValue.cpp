230023ab5d2f6eea9b55672e11651505f9ccedca
Thomas Smith <thsm@umich.edu>
2018-11-19 13:43:06 -0500

    Add inline documentation to NumberValues

--- src/NumberValue.cpp
+++ src/NumberValue.cpp
@@ -1,25 +1,42 @@
+// File: src/NumberValue.cpp
+// Purpose: A NumberValue is a Value that contains a floating point number.
+//  The actual C++ double inside the class should be accessed by built in
+//  functions. Fleet code can build more functions from these built in
+//  functions. For more documentation see src/NumberValue.hpp.
+
 #include <stdexcept>
 #include <string>
 #include "NumberValue.hpp"
 #include "TypeError.hpp"
 #include "Value.hpp"
 
+// Constructors
 NumberValue::NumberValue(double rawNumber): number(rawNumber) {}
 NumberValue::NumberValue(const NumberValue &other): number(other.number) {}
 NumberValue::NumberValue(const Token &numberToken):
   number(stod(numberToken.getValue())) {}
+
+// call([unused] arg) - Returns an error, since NumberValues cannot be called.
 Value::OrError NumberValue::call([[maybe_unused]] Value::Pointer arg) const {
   return { std::runtime_error { std::string {
     "Value of type "
   } + name + " cannot be called" } };
 }
+
+// operator string() - Returns the string representation of the internally
+//  stored double.
 NumberValue::operator std::string() const {
   return std::to_string(number);
 }
+
+// getRawNumber() - Returns the internal double value in the object.
 double NumberValue::getRawNumber() const {
   return number;
 }
+
 const std::string NumberValue::name { "Number" };
+
+// getName() - Returns "Number", the name of the Number type.
 std::string NumberValue::getName() const {
   return NumberValue::name;
 }
--- src/NumberValue.hpp
+++ src/NumberValue.hpp
@@ -1,3 +1,9 @@
+// File: src/NumberValue.hpp
+// Purpose: A NumberValue is a Value that contains a floating point number.
+//  The actual C++ double inside the class should be accessed by built in
+//  functions. Fleet code can build more functions from these built in
+//  functions. For implementations see src/NumberValue.cpp.
+
 #ifndef NUMBERVALUE_HPP
 #define NUMBERVALUE_HPP
 
@@ -9,13 +15,29 @@ class NumberValue: public Value {
 private:
   double number;
 public:
+  // Constructor(rawNumber) - Creates a NumberValue with rawNumber as its
+  //  internal C++ double.
   NumberValue(double rawNumber);
+  // Constructor(other) - Creates a copy of other.
   NumberValue(const NumberValue &other);
+  // Constructor(numberToken) - Creates a NumberValue with its internal number
+  //  as the value of the token.
   NumberValue(const Token &numberToken);
+
+  // Destructor - Default
   ~NumberValue() = default;
+
+  // call(arg) - Returns an error, since NumberValues cannot be called.
   Value::OrError call([[maybe_unused]] Value::Pointer arg) const;
+
+  // operator string() - Returns the string reprentation of the NumberValue's
+  //  internal number.
   operator std::string() const;
+
+  // getRawNumber() - Returns the NumberValue's internal C++ double.
   double getRawNumber() const;
+
+  // name/getName() - Returns "Number", the name of a NumberValue-type value.
   static const std::string name;
   std::string getName() const;
 };
