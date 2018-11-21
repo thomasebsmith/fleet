845cced7d65368a68de4bf70bef8708b3206395c
Thomas Smith <thsm@umich.edu>
2018-11-21 13:11:52 -0500

    Add inline documentation for ParseErrors

--- src/ParseError.cpp
+++ src/ParseError.cpp
@@ -1,8 +1,16 @@
+// File: src/ParseError.cpp
+// Purpose: A ParseError is a type of runtime_error that occurs during the
+//  parsing of code (i.e. during tokenization or token tree formation). For
+//  more documentation, see src/ParseError.hpp.
+
 #include <stdexcept>
 #include <string>
 #include "ParseError.hpp"
 
+// Constructor
 ParseError::ParseError(std::string message): runtime_error(message) {}
+
+// what() - Returns the content of the error message, preceded by "ParseError: "
 const char *ParseError::what() const throw() {
   return (std::string("ParseError: ") + runtime_error::what()).c_str();
 }
--- src/ParseError.hpp
+++ src/ParseError.hpp
@@ -1,3 +1,8 @@
+// File: src/ParseError.hpp
+// Purpose: A ParseError is a type of runtime_error that occurs during the
+//  parsing of code (i.e. during tokenization or token tree formation). For
+//  implementations see src/ParseError.cpp.
+
 #ifndef PARSEERROR_HPP
 #define PARSEERROR_HPP
 
@@ -5,7 +10,11 @@
 
 class ParseError: public std::runtime_error {
 public:
+  // Constructor(message) - Creates a ParseError with the given message
   ParseError(std::string message);
+
+  // what() - Returns the ParseError's message, preceded by the string
+  //  "ParseError: ".
   virtual const char *what() const throw();
 };
 
