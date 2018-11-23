8d3a559dc94935413f68b1938000d04fd301bf94
Thomas Smith <thsm@umich.edu>
2018-11-23 12:50:11 -0500

    Add inline documentation for Tokens

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,22 +1,37 @@
+// File: src/Token.cpp
+// Purpose: A Token is a single "word" in a programming language. It could be
+//  an identifier, an operator, a parenthesis or bracket, a number, a string,
+//  a comment, or a line break. For more documentation, see src/Token.hpp.
+
 #include <string>
 #include "Token.hpp"
 
+// Constructors
 Token::Token(): value(""), type(Token::Type::Comment) {}
 Token::Token(std::string value, Token::Type type): value(value), type(type) {}
 
+// getType() - Returns the type of the Token.
 Token::Type Token::getType() const {
   return type;
 }
+
+// getValue() - Returns the value of the Token.
 std::string Token::getValue() const {
   return value;
 }
+
+// setType(newType) - Sets the Token's type to newType.
 void Token::setType(Token::Type newType) {
   type = newType;
 }
+
+// setValue(newValue) - Sets the Token's value to newValue.
 void Token::setValue(std::string newValue) {
   value = newValue;
 }
 
+// isOpeningGrouper() - Returns a boolean indicating whether the Token is a
+//  an opening grouper (i.e. [, (, or { ).
 bool Token::isOpeningGrouper() const {
   if (getType() != Type::Grouper) {
     return false;
@@ -25,8 +40,8 @@ bool Token::isOpeningGrouper() const {
   return value == "(" || value == "[" || value == "{";
 }
 
-// Note that this function returns a copy of the original token if it is not an
-// opening grouper.
+// matchingGrouper() - Returns the matching grouper for a given Token, or a copy
+//  of the original Token if the Token is not a grouper.
 Token Token::matchingGrouper() const {
   std::string opener = getValue();
   Type type = getType();
@@ -51,12 +66,19 @@ Token Token::matchingGrouper() const {
   return Token { matching, type };
 }
 
+// ==rhs - Returns true iff the Token's value and type are the same as rhs's
+//  value and type.
 bool Token::operator==(const Token &rhs) const {
   return getType() == rhs.getType() && getValue() == rhs.getValue();
 }
+
+// !=rhs - Returns the opposite of ==rhs.
 bool Token::operator!=(const Token &rhs) const {
   return getType() != rhs.getType() || getValue() != rhs.getValue();
 }
+
+// operator string() - Returns a string representation of the Token in the form
+//  (type: value).
 Token::operator std::string() const {
   Token::Type t = getType();
   std::string typeString;
--- src/Token.hpp
+++ src/Token.hpp
@@ -1,3 +1,9 @@
+// File: src/Token.hpp
+// Purpose: A Token is a single "word" in a programming language. It could be
+//  an identifier, an operator, a parenthesis or bracket, a number, a string,
+//  a comment, or a line break. Each Token also contains a value - the string
+//  that formed the Token. For implementations, see src/Token.cpp.
+
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
@@ -19,16 +25,44 @@ private:
   Type type;
 
 public:
+  // Constructor() - Creates a Token containing an empty comment.
   Token();
+  
+  // Constructor(tokenValue, tokenType) - Creates a Token with the given
+  //  value and type.
   Token(std::string tokenValue, Type tokenType);
+
+  // getType() - Returns the Token's type.
   Type getType() const;
+
+  // getValue() - Returns the Token's value.
   std::string getValue() const;
+
+  // setType(newType) - Sets the Token's type to newType.
   void setType(Type newType);
+
+  // setValue(newValue) - Sets the Token's value to newValue.
   void setValue(std::string newValue);
+
+  // isOpeningGrouepr() - Returns a boolean that is true if and only if the
+  //  Token is a grouper and contains "(", "[", or "{".
   bool isOpeningGrouper() const;
+
+  // matchingGrouper() - Returns the matching grouper for a given grouper Token
+  //  (e.g. "(" for ")" or "}" for "{") or a copy of the Token if the Token
+  //  is not a grouper.
   Token matchingGrouper() const;
+
+  // ==rhs - Returns a boolean that is true if and only if the Token's type
+  //  and value are equal to rhs's type and value.
   bool operator==(const Token &rhs) const;
+
+  // !=rhs - Returns a boolean that is true if and only if the Token's type
+  //  or value is not equal to rhs's type or value.
   bool operator!=(const Token &rhs) const;
+
+  // operator string() - Returns a string representation of the Token in the
+  //  form "(type: value)".
   operator std::string() const;
 };
 
