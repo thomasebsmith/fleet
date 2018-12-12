6f507d3c3a68866e73be300d6431697d90852476
thsm <thsm@umich.edu>
2018-12-12 01:33:29 -0500

    Add inline documentation for TypeErrors

--- src/TypeError.cpp
+++ src/TypeError.cpp
@@ -1,8 +1,15 @@
+// File: TypeError.cpp
+// Purpose: Source file for TypeErrors, which are a type of runtime_error
+//  occurring when code mismatches types.
+
 #include <stdexcept>
 #include <string>
 #include "TypeError.hpp"
 
+// Constructor
 TypeError::TypeError(std::string message): runtime_error(message) {}
+
+// what() - Returns the content of the error message, preceded by "TypeError: "
 const char *TypeError::what() const throw() {
   return (std::string("TypeError: ") + runtime_error::what()).c_str();
 }
--- src/TypeError.hpp
+++ src/TypeError.hpp
@@ -1,3 +1,7 @@
+// File: TypeError.hpp
+// Purpose: Header file for TypeErrors, which are a type of runtime_error
+//  occurring when code mismatches types.
+
 #ifndef TYPEERROR_HPP
 #define TYPEERROR_HPP
 
@@ -5,7 +9,11 @@
 
 class TypeError: public std::runtime_error {
 public:
+  // Constructor(message) - Creates a TypeError with the given internal message.
   TypeError(std::string message);
+
+  // what() - Returns the internal message of the TypeError, preceded by
+  //  "TypeError: ".
   virtual const char *what() const throw();
 };
 
