7a92f7a06d351be96f2e04ad1d3b5e5f16a86080
thsm <thsm@umich.edu>
2018-12-12 01:25:58 -0500

    Add inline documentation for TokenTrees

--- src/TokenTree.cpp
+++ src/TokenTree.cpp
@@ -1,3 +1,11 @@
+// File: src/TokenTree.cpp
+// Purpose: Source file for TokenTrees, which represent a structured tree of
+//  Fleet Tokens. A TokenTree can be composed of one or more lines of
+//  TokenTrees, a function and its argument, or a Token. Technically, TokenTrees
+//  can also be "implied" - i.e. they represent implicit arguments to a function
+//  such as the first argument in `(+ 3)`. For more documentation, see
+//  src/TokenTree.hpp.
+
 #include <memory>
 #include <optional>
 #include <stack>
@@ -13,6 +21,7 @@
 #include "TokenTree.hpp"
 #include "TokenTreeVisitor.hpp"
 
+// Constructors
 TokenTree::TokenTree(const Token &value): data { value } {}
 
 TokenTree::TokenTree(
@@ -22,14 +31,14 @@ TokenTree::TokenTree(
 TokenTree::TokenTree(const TokenTree::LineList &lines):
   data { lines } {}
 
-TokenTree::TokenTree(const TokenTree &copyFrom):
-  data { copyFrom.data } {}
-
 TokenTree::TokenTree(): data { std::monostate {} } {}
 
+// The table of precedences for each operator. Precedences cannot be changed
+//  during parsing.
 // *Nothing* should have a precedence less than 0, since that is reserved for
-// function calls internally.
-int TokenTree::defaultPrecedence = 50;
+//  function calls internally.
+// Operators not specified in this table have a precedence of 60.
+int TokenTree::defaultPrecedence = 60;
 std::unordered_map<std::string, int> TokenTree::precedences = {
   { ".", 100},
   { ":", 90 },
@@ -49,13 +58,14 @@ std::unordered_map<std::string, int> TokenTree::precedences = {
   { "=", 0  }
 };
 
-// Default associativity = true (left-associative)
-// false = right-associative
+// The table of associativities for each operator. They default to being left-
+//  associative (a value of `true`). Only `^` is right-associative right now.
 std::unordered_map<std::string, bool> TokenTree::associativities {
   { "^", false }
 };
-bool TokenTree::defaultAssociativity = true;
+bool TokenTree::defaultAssociativity = true; // Left-associative
 
+// This function returns the precedence level of a given operator string.
 int TokenTree::getPrecedence(std::string op) {
   if (TokenTree::precedences.count(op) > 0) {
     return TokenTree::precedences.at(op);
@@ -63,6 +73,9 @@ int TokenTree::getPrecedence(std::string op) {
   return TokenTree::defaultPrecedence;
 }
 
+// This function returns the associativity of a given operator string as a
+//  boolean, where `true` represents left-associativity and `false` represents
+//  right-associativity.
 bool TokenTree::getAssociativity(std::string op) {
   if (TokenTree::associativities.count(op) > 0) {
     return TokenTree::associativities.at(op);
@@ -70,13 +83,17 @@ bool TokenTree::getAssociativity(std::string op) {
   return TokenTree::defaultAssociativity;
 }
 
+// This function returns an optional that contains a Token iff this TokenTree
+//  is a leaf (i.e. it contains only one Token).
 std::optional<Token> TokenTree::getToken() const {
   if (std::holds_alternative<Token>(data)) {
     return { *std::get_if<Token>(&data) };
   }
   return {};
 }
-  
+
+// This function returns an optional that contains a pair of TokenTrees iff
+//  this TokenTree is a function call.
 std::optional<
   std::pair<TokenTree, TokenTree>
 > TokenTree::getFunctionPair() const {
@@ -90,6 +107,8 @@ std::optional<
   return {};
 }
 
+// This function returns an optional that contains a vector of TokenTrees iff
+//  this TokenTree is a list of lines of code.
 std::optional<std::vector<TokenTree>> TokenTree::getLineList() const {
   if (std::holds_alternative<TokenTree::LineList>(data)) {
     const auto &list = *std::get_if<TokenTree::LineList>(&data);
@@ -107,11 +126,17 @@ std::optional<std::vector<TokenTree>> TokenTree::getLineList() const {
   return {};
 }
 
+// This function returns a boolean indicating whether this TokenTree represents
+//  an implied argument to a function.
 bool TokenTree::isImplied() const {
   return std::holds_alternative<std::monostate>(data);
 }
 
+// This operator returns a boolean indicating whether this TokenTree is
+//  equivalent to another TokenTree. To be true, both TokenTrees must be of the
+//  same variant, and they must each contains values that equal each other.
 bool TokenTree::operator==(const TokenTree &rhs) const {
+  // Check to see if both are equivalent Tokens.
   const auto &leftToken = getToken();
   const auto &rightToken = rhs.getToken();
   if (leftToken || rightToken) {
@@ -120,6 +145,8 @@ bool TokenTree::operator==(const TokenTree &rhs) const {
     }
     return *leftToken == *rightToken;
   }
+
+  // Check to see if both are equivalent function pairs.
   const auto &leftPair = getFunctionPair();
   const auto &rightPair = rhs.getFunctionPair();
   if (leftPair || rightPair) {
@@ -128,6 +155,8 @@ bool TokenTree::operator==(const TokenTree &rhs) const {
     }
     return *leftPair == *rightPair;
   }
+
+  // Check to see if both are equivalent line lists.
   const auto &leftLines = getLineList();
   const auto &rightLines = rhs.getLineList();
   if (leftLines || rightLines) {
@@ -136,19 +165,29 @@ bool TokenTree::operator==(const TokenTree &rhs) const {
     }
     return *leftLines == *rightLines;
   }
+
+  // Return true in all other cases (should only occur if both are implied).
   return true;
 }
 
+// This operator returns the string representation of a given TokenTree. The
+//  string representation is formed as [Token], [f, x], or {line; line...}
+//  depending on the variant of TokenTree.
 TokenTree::operator std::string() const {
+  // Tokens are represented as [Token].
   const auto &token = getToken();
   if (token) {
     return std::string("[") + static_cast<std::string>(*token) + "]";
   }
+
+  // Function pairs are represented as [f, x].
   const auto &pair = getFunctionPair();
   if (pair) {
     return std::string("[") + static_cast<std::string>(pair->first) +
       ", " + static_cast<std::string>(pair->second) + "]";
   }
+
+  // Line lists are represented as {lines...}.
   const auto &lines = getLineList();
   if (lines) {
     std::string result = "{";
@@ -161,9 +200,14 @@ TokenTree::operator std::string() const {
     result += "}";
     return result;
   }
-  return "{}";
+
+  // Implied arguments are represented as <implied>.
+  return "<implied>";
 }
 
+// This function constructs a TokenTree from a given TokenStream. It uses a form
+//  of the shunting-yard algorithm for operator precedence parsing modified to
+//  parse Fleet-style function calls (i.e. function calls of the form `f x`).
 TokenTree TokenTree::build(TokenStream stream) {
   std::stack<std::tuple<Token, int, bool>> operatorStack;
   std::stack<bool> lastWasNonOperatorStack;
@@ -181,6 +225,11 @@ TokenTree TokenTree::build(TokenStream stream) {
       case Token::Type::Number:
       case Token::Type::String:
         if (lastWasNonOperatorStack.top()) {
+          // If the last stream Token was not an operator, then this is a
+          //  function call, so create a TokenTree with the function and its
+          //  argument.
+
+          // The function should have already been pushed to the output queue.
           if (outputQueue.empty()) {
             throw ParseError("Internal parsing error");
           }
@@ -198,10 +247,14 @@ TokenTree TokenTree::build(TokenStream stream) {
         break;
       case Token::Type::Grouper:
         if (next.isOpeningGrouper()) {
+          // If the stream Token is (, [, or {, push the grouper onto the
+          //  operator stack and enter a new layer of parsing.
           operatorStack.push({ next, 0, false });
           lastWasNonOperatorStack.push(false);
         }
         else {
+          // If the stream Token is ), ], or }, apply operators from the
+          //  operator stack until a matching grouper is found.
           Token t;
           bool grouperWasClosed = false;
           while (!operatorStack.empty()) {
@@ -209,12 +262,24 @@ TokenTree TokenTree::build(TokenStream stream) {
             operatorStack.pop();
             if (t.getType() == Token::Type::Grouper) {
               if (t.matchingGrouper() != next) {
+                // If a non-matching grouper is found first, throw a parse
+                //  error.
                 throw ParseError("Unmatched " + t.getValue());
               }
               grouperWasClosed = true;
               break;
             }
             else {
+              // Apply the operator to the output queue.
+              // If there are no items in the output queue, simply push the
+              //  operator onto the output queue.
+              // If there is one item in the output queue and it is implied,
+              //  push the operator onto output queue.
+              // If there is one item in the output queue and it is not implied,
+              //  push the operator with one argument applied onto the output
+              //  queue.
+              // Otherwise, push the operator with two arguments applied onto
+              //  the output queue.
               if (outputQueue.empty()) {
                 outputQueue.emplace_back(new TokenTree { t });
               }
@@ -270,6 +335,8 @@ TokenTree TokenTree::build(TokenStream stream) {
         }
         break;
       case Token::Type::LineBreak:
+        // If a line break is encountered, evaluate all operators (see comments
+        //  above for details) and add the result to the vector of lines.
         while (!lastWasNonOperatorStack.empty()) {
           lastWasNonOperatorStack.pop();
         }
@@ -322,6 +389,9 @@ TokenTree TokenTree::build(TokenStream stream) {
         }
         break;
       case Token::Type::Operator: {
+        // If an operator is encountered and the output queue is empty (i.e.
+        //  the operator is the first token encountered in this grouping), then
+        //  add an implied first argument.
         lastWasNonOperatorStack.top() = false;
         int precedence = getPrecedence(next.getValue());
         bool associativity = getAssociativity(next.getValue());
@@ -331,6 +401,9 @@ TokenTree TokenTree::build(TokenStream stream) {
           });
         }
 
+        // Apply operators until all Tokens are exhausted or until an operator
+        //  with a lower precedence (or equal precedence if right-associative)
+        //  is encountered.
         while (!operatorStack.empty() &&
           !std::get<0>(operatorStack.top()).isOpeningGrouper() && (
           std::get<1>(operatorStack.top()) > precedence ||
@@ -376,10 +449,14 @@ TokenTree TokenTree::build(TokenStream stream) {
         }
         break;
       default:
-        // Comments are ignored
+        // Comments are ignored.
         continue;
     }
   }
+
+  // Once all Tokens from the TokenStream are exhausted, empty the operator
+  //  stack and output queue by applying operators and groupers (see above
+  //  comments).
   if (!outputQueue.empty() || !operatorStack.empty()) {
     Token t;
     while (!operatorStack.empty()) {
@@ -422,5 +499,8 @@ TokenTree TokenTree::build(TokenStream stream) {
     lines.push_back({outputQueue.back()});
     outputQueue.pop_back();
   }
+
+  // Return a TokenTree containing all the lines of TokenTrees that were
+  //  created.
   return { lines };
 }
--- src/TokenTree.hpp
+++ src/TokenTree.hpp
@@ -1,3 +1,11 @@
+// File: src/TokenTree.cpp
+// Purpose: Source file for TokenTrees, which represent a structured tree of
+//  Fleet Tokens. A TokenTree can be composed of one or more lines of
+//  TokenTrees, a function and its argument, or a Token. Technically, TokenTrees
+//  can also be "implied" - i.e. they represent implicit arguments to a function
+//  such as the first argument in `(+ 3)`. For implementations, see
+//  src/TokenTree.cpp.
+
 #ifndef TOKENTREE_HPP
 #define TOKENTREE_HPP
 
@@ -15,6 +23,8 @@
 class TokenTree {
 public:
 
+  // These typedefs can be used to refer to the various states of a TokenTree
+  //  succinctly.
   typedef std::shared_ptr<TokenTree> TreePointer;
   typedef std::pair<TreePointer, TreePointer> FunctionPair;
   typedef std::vector<TreePointer> LineList;
@@ -34,12 +44,23 @@ private:
 
 public:
 
+  // Constructor(value) - Constructs a TokenTree from a single given Token.
   TokenTree(const Token &value);
+  
+  // Constructor(f, x) - Constructs a TokenTree as a function call with function
+  //  f and argument x.
   TokenTree(const TreePointer &f, const TreePointer &x);
+
+  // Constructor(lines) - Constructs a TokenTree as a list of the given
+  //  TokenTrees.
   TokenTree(const LineList &lines);
-  TokenTree(const TokenTree &copyFrom);
+
+  // Constructor() - Constructs an implied argument TokenTree.
   TokenTree();
 
+  // accept(v) - Following the visitor paradigm, calls the appropriate visit
+  //  method on v based on the contents of this TokenTree. Returns the same
+  //  type as the visit method on v.
   template <typename T>
   T accept(const TokenTreeVisitor<T> &v) const {
     const auto &token = getToken();
@@ -60,16 +81,45 @@ public:
     throw ParseError { "Internal error: Unable to accept TokenTree visitor" };
   }
 
+  // getToken() - Returns a Token iff this TokenTree contains a Token.
   std::optional<Token> getToken() const;
+
+  // getFunctionPair() - Returns a pair {f, x} iff this TokenTree contains a
+  //  function pair.
   std::optional<std::pair<TokenTree, TokenTree>> getFunctionPair() const;
+
+  // getLineList() - Returns a vector of lines iff this TokenTree contains a
+  //  number of lines.
   std::optional<std::vector<TokenTree>> getLineList() const;
+
+  // isImplied() - Returns a boolean indicating whether this TokenTree is
+  //  implied.
   bool isImplied() const;
 
+  // ==rhs - Returns a boolean indicating whether this TokenTree is equivalent
+  //  to rhs, which only occurs when both are the same variant and their
+  //  contents are equivalent.
   bool operator==(const TokenTree &rhs) const;
+
+  // std::string - Returns a string representation of this TokenTree.
+  //  For a Token, this is [token_value].
+  //  For a function pair, this is [f, x].
+  //  For a line list, this is {line,line,...}.
+  //  For an implied argument, this is <implied>.
   operator std::string() const;
 
+  // static getPrecedence(op) - Returns the predence of an operator string op.
+  //  This is based on hard-coded values for special operators, defaulting to 60
+  //  for other operators.
   static int getPrecedence(std::string op);
+
+  // static getAssociativity(op) - Returns the associativity of an operator
+  //  string op. This defaults to true (left-associative). A return value of
+  //  false represents right-associativity.
   static bool getAssociativity(std::string op);
+
+  // static build(stream) - Builds a TokenTree form the given TokenStream. For
+  //  details of this function's workings, see src/TokenTree.cpp.
   static TokenTree build(TokenStream stream);
 };
 
