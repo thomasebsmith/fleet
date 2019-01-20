b601faa75fb5fa19b372362bfe942973fab12e41
thsm <thsm@umich.edu>
2019-01-19 23:19:52 -0500

    Add more Evaluator tests

--- tests/TestEvaluator.cpp
+++ tests/TestEvaluator.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <variant>
 #include "TestEvaluator.hpp"
 #include "Context.hpp"
@@ -8,21 +9,39 @@
 #include "Value.hpp"
 
 // Function prototypes
+bool evaluatesToNumber(const Evaluator &eval, std::string code, double num);
 void testRawNumbers();
+void testWhitespace();
 
 int TestEvaluator::main() {
   Tester tester("Evaluator tests");
   tester.test("Test raw numbers", testRawNumbers);
+  tester.test("Test whitespace", testWhitespace);
   return tester.run();
+}
+
+bool evaluatesToNumber(Evaluator &eval, std::string code, double num) {
+  const auto result = eval.evaluate(TokenTree::build({ code }));
+  if (!std::holds_alternative<Value::Pointer>(result)) {
+    return false;
+  }
+  const auto evaledNum = (*std::get_if<Value::Pointer>(&result))->
+    castValue<NumberValue>();
+  if (!evaledNum) {
+    return false;
+  }
+  return evaledNum->getRawNumber() == num;
 }
 
 void testRawNumbers() {
   Evaluator eval { new Context() };
-  TokenStream stream { "2" };
-  TokenTree tree = TokenTree::build(stream);
-  const auto result = eval.evaluate(tree);
-  Tester::confirm(std::holds_alternative<Value::Pointer>(result));
-  const auto num = (*std::get_if<Value::Pointer>(&result))->
-    castValue<NumberValue>();
-  Tester::confirm(num->getRawNumber() == 2.0);
+  Tester::confirm(evaluatesToNumber(eval, "2", 2.0));
+  Tester::confirm(evaluatesToNumber(eval, "57.488", 57.488));
+}
+
+void testWhitespace() {
+  Evaluator eval { new Context() };
+  Tester::confirm(evaluatesToNumber(eval, " \t8.8", 8.8));
+  Tester::confirm(evaluatesToNumber(eval, "10.00000 \n \n\n\t ", 10.0));
+  Tester::confirm(evaluatesToNumber(eval, "\f87.65\t\t\n  ", 87.65));
 }
