580ce693d6cce478e6259d9d6430cce135705a42
thsm <thsm@umich.edu>
2019-01-31 23:47:58 -0500

    Add inline documentation to tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,6 @@
+// File: tests/tests.cpp
+// Purpose: Source file for the main test function, which runs all tests and
+//  outputs a summary of the results.
 #include <iostream>
 #include "TestContext.hpp"
 #include "TestEvaluator.hpp"
@@ -5,6 +8,9 @@
 #include "TestTokenStream.hpp"
 #include "TestTokenTree.hpp"
 
+// main() - Runs all the tests specified in the code below. Adds up the number
+//  of failed tests, and returns that number. If all tests passed, it outputs
+//  that fact. If some tests failed, it outputs the number of tests failed.
 int main() {
   int result =
     TestToken::main() + TestTokenStream::main() + TestTokenTree::main() +
